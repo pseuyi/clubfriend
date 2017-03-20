1e08a2b26eb015234ec3b9258c4eaba7d4608f68
freda nada <fredasnada@gmail.com>
2017-03-20 10:29:00 -0400

    highlight link

--- client/components/Desc.jsx
+++ client/components/Desc.jsx
@@ -12,11 +12,11 @@ export default class Desc extends React.Component {
             id (int), name (string), and favorite city (string).<br />
           the following endpoints are supported and return a JSON object containing a "users" array:</p>
         <p>
-          /GET request to <a href={"http://clubfriend.herokuapp.com/people"}>/people</a> returns all user objects<br />
-        /POST request to /people creates a new user object<br />
-        /PUT request to /people updates a user's info<br />
-        /GET request to <a href={"http://clubfriend.herokuapp.com/people/100"}>/people/:id</a> returns a specific user's info<br />
-        /DELETE request to /people/:id deletes a specific user
+          /GET request to <a style={{color: 'pink'}} href={"http://clubfriend.herokuapp.com/people"}>/people</a> returns all user objects<br />
+          /POST request to /people creates a new user object<br />
+          /PUT request to /people updates a user's info<br />
+          /GET request to <a style={{color: 'pink'}} href={"http://clubfriend.herokuapp.com/people/100"}>/people/:id</a> returns a specific user's info<br />
+          /DELETE request to /people/:id deletes a specific user
         </p>
         </div>
       </div>
